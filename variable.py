f6be29333c41205ebbdd91674720a0a3febd73c2
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-15 00:35:13 +0530

    Added basic variable example in Python

---
+++ variable.py
@@ -0,0 +1,12 @@
+# This is a simple Python variable example
+
+name = "Kalpesh"        # String variable
+age = 20                # Integer variable
+height = 5.8            # Float variable
+is_student = True       # Boolean variable
+
+# Print the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
