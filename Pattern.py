b6d4f8ee52ed938ae9260bc10b950d89f87d2d94
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-15 00:55:43 +0530

    Adding some pattern example using loop

---
+++ Pattern.py
@@ -0,0 +1,54 @@
+# simple pattern printing
+
+# for i in range(3):
+#     for j in range(2):
+#         print("*",end="")
+#     print("*")
+
+# take input from user
+
+# n = int(input())
+# m = int(input())
+
+# for row in range(n):
+#     for column in range(m):
+#         print(column,end="")
+#     print("")
+
+
+# num = 1
+# for i in range(1,7):
+#     for j in range(i):
+#         print("*",end="")
+#         num += 1
+#     print()
+   
+
+# n = int(input())
+# m = int(input())
+# for i in range(n):
+#     for j in range(m):
+#         print(j,end="")
+#     print(i)
+   
+# num = 1
+# n =int(input())
+# for i in range(1,n-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print("")
+
+# size= 10
+# for i in range(size):
+#     print("*" * size)
+
+size= 10
+for i in range(size):
+    print("*" * (i+1))
+
+
+
+
+        
+ 
+    
