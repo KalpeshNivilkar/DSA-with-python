f21da737dd6b63ec1164cce19632430792f57943
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-15 14:40:45 +0530

    creating code using jumpstatement such as break and continue

---
+++ JumpStatement.py
@@ -0,0 +1,22 @@
+# Jump statements are used to change the flow of a program.
+# 1. break statment
+# print("this is break statement")
+# for i in range(5):
+#     if i ==3:
+#         break
+#     print(i)
+
+# print("this is continue statement")
+# for j in range(5):
+#     if j == 4:
+#         continue
+#     print(j)
+
+
+# Question:
+# Write a program using a for loop that prints numbers from 1 to 10, but stops the loop when the number is 5.
+
+for k in range(1,10):
+    if k==5:
+        continue
+    print(k)
