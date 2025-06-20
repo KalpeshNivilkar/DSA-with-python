b05e48b7cdecbefb62669901c853a5dc2870ef90
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-21 01:55:14 +0530

    add new examples

--- list.py
+++ list.py
@@ -12,18 +12,18 @@
 
 # taking input from user  and do sum of user input
  
-n = int(input("enter how much element you want to store :"))
-list = []
-sum = 0
-                                                                                  # taking input from user and do sum of user input 
-for i in range(n):
-    num = int(input(f"enter elements :"))
-    list.append(num)
-    sum += num
+# n = int(input("enter how much element you want to store :"))
+# list = []
+# sum = 0
+#                                                                                   # taking input from user and do sum of user input 
+# for i in range(n):
+#     num = int(input(f"enter elements :"))
+#     list.append(num)
+#     sum += num
 
-list.sort()
-print(list)
-print(sum)
+# list.sort()
+# print(list)
+# print(sum)
 
 
 # finding min and max from list
@@ -37,5 +37,59 @@ print(sum)
 # print(mylist)
 
 
-mylist = ["kalpesh","avinash"]
-print(mylist)
+# practical question 
+
+# 1] wrie a prigram to  ask the user  to enter their three movies  and store items into the list 
+
+# movies = []
+
+# mov1 = input("enter 1st movie :")
+# mov2 = input("enter 2st movie :")
+# mov3 = input("enter 3st movie :")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+    
+# another way to append 
+
+# movies = []
+
+# movies.append(input("enter 1st movie :"))
+# movies.append(input("enter 1st movie :"))
+# movies.append(input("enter 1st movie :"))
+
+# print(movies)
+
+# 2] palidrone value check 
+
+# checkList = [1,2,3,2,1]
+# copyl = checkList.copy()
+# copyl.reverse()
+# print(copyl)
+
+# if checkList == copyl:
+#     print("this list is palindrome")
+# else:
+#     print("this list is not palindrome")
+
+# 3] find largest number in list 
+
+# list = [13,45,34,23]
+
+# findLargestNumber = max(list)
+# print(findLargestNumber)
+
+# 4] Count how many times a number appears in a list.
+list = [1, 2, 2, 3, 2]
+countlist = list.count(2)
+print(countlist)
+    
+
+
+
+
+
+
