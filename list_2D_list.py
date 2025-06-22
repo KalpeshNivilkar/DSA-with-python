b120f8ae3f37857e67413935fcad2f72843a7ee9
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-22 21:50:09 +0530

    add and practice 2d list code

---
+++ list_2D_list.py
@@ -0,0 +1,33 @@
+# 2D list used for store element in matrix form 
+
+# for example 
+
+n = int(input())
+m = int(input())
+
+grid= []
+for i in range(n):
+    item  = [ele for ele in input().split()]
+    grid.append(item)
+
+print(grid)
+
+# for example 
+
+# n = int(input())
+# m = int(input())
+
+# grid = []
+# for i in range(n):
+#     item = [ele for ele in input().slice()]
+#     grid.append(item)
+
+# print(grid)
+
+
+
+
+
+
+
+
