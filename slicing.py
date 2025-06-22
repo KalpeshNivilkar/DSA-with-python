3ad2fe60bebc0a76900e5ca37fdf189c92fdbe4e
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-22 09:14:35 +0530

    adding list slicing method

---
+++ slicing.py
@@ -0,0 +1,14 @@
+# syntax of slicing 
+# mylist = [start:end:step] 
+
+# for example 
+
+myList = [12,34,45,56,67,78]
+print(myList[1:len(myList):2])
+
+# output will be : [34, 56, 78]
+
+# another example 
+
+yourList = [10,20,30,40,50]
+print(yourList[::-1])
