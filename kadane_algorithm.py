7adc107becb9ccf4596a4a8a0bd38c5d4f763b3b
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-25 01:35:36 +0530

    add new code

--- kadane_algorithm.py
+++ kadane_algorithm.py
@@ -1,20 +1,4 @@
-# def max_subarray(nums):
-#      currSum = 0
-#      maxSum = float('-inf') 
-
-#      for i in nums:
-#           currSum += i
-#           maxSum = max(maxSum,currSum)
-
-#           if currSum < 0:
-#                currSum = 0
-    
-    
-# arr = [-2,1,-3,4,-1,2,1,-5,4]
-# print(max_subarray(arr))
-
-
-
+# largest subarray sum using kadane algorithm 
 
 def max_subarray(nums):
     currSum = 0
@@ -27,8 +11,31 @@ def max_subarray(nums):
         if currSum < 0:
             currSum = 0
 
-    return maxSum  # <-- return the result!
+    return maxSum  
 
-# Test it:
+
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(max_subarray(arr))  # Output: 6
+print(max_subarray(arr))  
+# Output: 6
+
+# leetcode ans :
+
+# class Solution(object):
+#     def maxSubArray(self, nums):
+#         currSum = 0
+#         maxSum = float('-inf')
+
+#         for i in nums:
+#             currSum += i
+#             maxSum = max(maxSum, currSum) 
+
+#             if currSum < 0:
+#                 currSum = 0
+
+#         return maxSum  
+
+
+# sol = Solution()          
+# arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# print(sol.maxSubArray(arr))  
+
