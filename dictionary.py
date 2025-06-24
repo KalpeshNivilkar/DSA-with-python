7188bb8b4319b0c508078586420d9d4166484d9d
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-24 13:19:17 +0530

    add new file

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+# *dictionary : are used to store data in key:value pairs 
+# method 1 : key()
+# method 2 : values()
+
+
+# for example 
+myInfo = {
+    "key":"value",
+    "name":"kalpesh",
+    "age": 21,
+    "add":"gokulnagar",
+    "sub":["python","c","C++"]
+
+}
+print(myInfo)
+
+print(myInfo.keys())
+
+print(myInfo.values())
+
+
+# practise question 
+
+dict = {"table": ["a piece of furniture","list of fact and figure"],
+        "cat":"a small animal"}
+
+print(dict)
