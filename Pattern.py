c47974b93595ea41a477175309e491b5e9078c69
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-06-30 23:47:58 +0530

    add new file

--- Pattern.py
+++ Pattern.py
@@ -72,7 +72,5 @@
 # *
 
 
-for i in range(4):
-    print(i*i)
 
     
--- kadane_algorithm.py
+++ kadane_algorithm.py
@@ -1,21 +1,21 @@
 # largest subarray sum using kadane algorithm 
 
-def max_subarray(nums):
-    currSum = 0
-    maxSum = float('-inf')  # start lower than any possible sum
+# def max_subarray(nums):
+#     currSum = 0
+#     maxSum = float('-inf')  # start lower than any possible sum
 
-    for i in nums:
-        currSum += i
-        maxSum = max(maxSum, currSum)
+#     for i in nums:
+#         currSum += i
+#         maxSum = max(maxSum, currSum)
 
-        if currSum < 0:
-            currSum = 0
+#         if currSum < 0:
+#             currSum = 0
 
-    return maxSum  
+#     return maxSum  
 
 
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(max_subarray(arr))  
+# arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# print(max_subarray(arr))  
 # Output: 6
 
 # leetcode ans :
@@ -38,4 +38,9 @@ print(max_subarray(arr))
 # sol = Solution()          
 # arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 # print(sol.maxSubArray(arr))  
+
+
+
+
+
 
