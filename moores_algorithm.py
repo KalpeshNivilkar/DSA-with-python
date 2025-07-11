c49a3e39ae78a1f94da903b6cbcbc640f4bba036
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-07-11 13:49:33 +0530

    add code

--- moores_algorithm.py
+++ moores_algorithm.py
@@ -1,14 +1,20 @@
 def majorityElement(num):
     n = len(num)
-    for i in num:
-        freq = 0
-        for j in num:
-            if i == j:
-                freq += 1
-        
-        if freq >= n //2:
-            return i
-        
+    freq = 0
+    ans = 0
 
-arr = [1,2,3,1,1,1,2]
+    for i in num:
+        if freq == 0:
+            ans = i
+        # Count matching vs non-matching elements
+        if i == ans:
+            freq += 1
+        else:
+            freq -= 1
+
+    return ans  # Moved outside the loop
+
+   
+
+arr = [1,2,2,2,2,2,2,3,1,1,1,2]
 print(majorityElement(arr))
