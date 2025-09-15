2621d5365871ef2459203b3ec35b9359e2eb161e
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-09-16 00:30:36 +0530

    add new code

--- variable.py
+++ variable.py
@@ -1,12 +1,23 @@
-# This is a simple Python variable example
+# # This is a simple Python variable example
 
-name = "Kalpesh"        # String variable
-age = 20                # Integer variable
-height = 5.8            # Float variable
-is_student = True       # Boolean variable
+# name = "Kalpesh"        # String variable
+# age = 20                # Integer variable
+# height = 5.8            # Float variable
+# is_student = True       # Boolean variable
 
-# Print the variables
-print("Name:", name)
-print("Age:", age)
-print("Height:", height)
-print("Is Student:", is_student)
+# # Print the variables
+# print("Name:", name)
+# print("Age:", age)
+# print("Height:", height)
+# print("Is Student:", is_student)
+
+
+# age = 20
+
+# print("your age is", age +5, "now")
+
+x = 15
+y = 30
+
+x, y = y, x
+print(x, y)
