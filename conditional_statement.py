0708e9162714ca46e54814bbfd5f85f5c7a321ed
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-09-16 23:05:00 +0530

    add new code

---
+++ conditional_statement.py
@@ -0,0 +1,7 @@
+# Check if a number is positive, negative, or zero.
+# number = int(input("enter any number :"))
+
+# if number < 0:
+#     print(number, "is negative")
+# else:
+#     print(number, "is positive")
--- dictionary.py
+++ dictionary.py
@@ -1,38 +1,48 @@
-# *dictionary : are used to store data in key:value pairs 
-# method 1 : key()
-# method 2 : values()
+# # *dictionary : are used to store data in key:value pairs 
+# # method 1 : key()
+# # method 2 : values()
 
 
-# for example 
-myInfo = {
-    "key":"value",
-    "name":"kalpesh",
-    "age": 21,
-    "add":"gokulnagar",
-    "sub":["python","c","C++"]
+# # for example 
+# myInfo = {
+#     "key":"value",
+#     "name":"kalpesh",
+#     "age": 21,
+#     "add":"gokulnagar",
+#     "sub":["python","c","C++"]
 
-}
-print(myInfo)
+# }
+# print(myInfo)
 
-print(myInfo.keys())
+# print(myInfo.keys())
 
-print(myInfo.values())
+# print(myInfo.values())
 
 
-# practise question 
+# # practise question 
 
-dict = {"table": ["a piece of furniture","list of fact and figure"],
-        "cat":"a small animal"}
+# dict = {"table": ["a piece of furniture","list of fact and figure"],
+#         "cat":"a small animal"}
 
-print(dict)
+# print(dict)
 
-# practise question 
+# # practise question 
 
-n = int(input("hoow much sub store :"))
+# n = int(input("hoow much sub store :"))
+# dict = {}
+# for i in range(n):
+#     sub = input("enter the sub :")
+#     marks = input("enter the marks :")
+#     dict[sub] = marks
+# print(dict)
+
+n = int(input("how much sub you have to store"))
+
 dict = {}
 for i in range(n):
-    sub = input("enter the sub :")
-    marks = input("enter the marks :")
+    sub = input("enter the sub")
+    marks = input("enter the marks")
+
     dict[sub] = marks
 print(dict)
 
