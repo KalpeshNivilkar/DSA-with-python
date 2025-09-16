e54faf99da7e4ad365aa893883bd322938504a41
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-09-16 13:23:43 +0530

    add new code

--- string.py
+++ string.py
@@ -1,104 +1,119 @@
-# string :  String is data type that stores a sequence of characters.
-        #   method 1 ] slicing : 
-        #                      str[ starting_idx : ending_idx ] #ending idx is not included
-        #   method 2 ] count :
-        #                     used to count the sting also single character 
-        #   method 3 ] split():
-        #                     used to split the string 
-        #   method 4 ] strip():
-        #                     used to removed extra empty space taken by string element
-        #   method 5 ] ASCII value : ord()
+# # string :  String is data type that stores a sequence of characters.
+#         #   method 1 ] slicing : 
+#         #                      str[ starting_idx : ending_idx ] #ending idx is not included
+#         #   method 2 ] count :
+#         #                     used to count the sting also single character 
+#         #   method 3 ] split():
+#         #                     used to split the string 
+#         #   method 4 ] strip():
+#         #                     used to removed extra empty space taken by string element
+#         #   method 5 ] ASCII value : ord()
 
-# printing lenght of string 
+# # printing lenght of string 
 
-name = "kalpesh nivilkar"
-nameLenght = len(name)
-print(nameLenght)
-# output will be : 16 
+# name = "kalpesh nivilkar"
+# nameLenght = len(name)
+# print(nameLenght)
+# # output will be : 16 
 
-# accessing index element 
+# # accessing index element 
+
+# access = name[1]
+# print(access)
+# # output will be : a 
+
+# # slicing of list 
+
+# # name [starting index : ending index]   ........syntax
+
+# sliceName = name[0 : 7]
+# print(sliceName)
+# # output will be : kalpesh 
+
+
+# # example]  WAP to find the occurrence of ‘$’ in a String.
+
+# # text = input("enter any text which conatain $ :")
+
+# # counttext = text.count("$")
+# # print(counttext)
+
+#             # using another way
+
+
+# # msg = str(input("enter the msg which contain $ : "))
+# # count = 0 
+
+# # for i in (msg):
+# #     if str == "$":
+# #         count += i
+
+# # print(count)
+
+
+
+# # split text 
+
+# mySelf = "kalpesh subhash nivilkar"
+# print(mySelf.split())
+# # output will be : ['kalpesh', 'subhash', 'nivilkar']
+
+# mySelf = "kalpesh subhash nivilkar"
+# print(mySelf.split("a"))
+# # output will be : ['k', 'lpesh subh', 'sh nivilk', 'r']
+
+
+
+# # strip method
+
+# yourSelf = "  adinath   "
+# print(yourSelf)
+# # output will be :   adinath   
+
+# print(yourSelf.strip())
+# # output will be :adinath  
+
+
+# # ASCII value 
+
+# # me = "a"
+# # print(ord(me))
+
+
+# # practice question 
+# # check palindrom 
+
+# # text = input("Enter the text : ")
+
+# # if text == text[::-1]:
+# #     print("the text is palidrom")
+# # else:
+# #     print("text is no palidrome")
+
+
+
+
+# # find num 5244 = 5*2*4*4
+
+# num = 5244
+# total = 1
+
+# for i in str(num):
+#     total *= int(i)
+
+# print(total)
+
+
+name = "kalpesh"
+lenofname = len(name)
+print(lenofname)
+
+#accessing name index
 
 access = name[1]
 print(access)
-# output will be : a 
 
-# slicing of list 
+#slice name
 
-# name [starting index : ending index]   ........syntax
-
-sliceName = name[0 : 7]
-print(sliceName)
-# output will be : kalpesh 
-
-
-# example]  WAP to find the occurrence of ‘$’ in a String.
-
-# text = input("enter any text which conatain $ :")
-
-# counttext = text.count("$")
-# print(counttext)
-
-            # using another way
-
-
-# msg = str(input("enter the msg which contain $ : "))
-# count = 0 
-
-# for i in (msg):
-#     if str == "$":
-#         count += i
-
-# print(count)
-
-
-
-# split text 
-
-mySelf = "kalpesh subhash nivilkar"
-print(mySelf.split())
-# output will be : ['kalpesh', 'subhash', 'nivilkar']
-
-mySelf = "kalpesh subhash nivilkar"
-print(mySelf.split("a"))
-# output will be : ['k', 'lpesh subh', 'sh nivilk', 'r']
-
-
-
-# strip method
-
-yourSelf = "  adinath   "
-print(yourSelf)
-# output will be :   adinath   
-
-print(yourSelf.strip())
-# output will be :adinath  
-
-
-# ASCII value 
-
-# me = "a"
-# print(ord(me))
-
-
-# practice question 
-# check palindrom 
-
-# text = input("Enter the text : ")
-
-# if text == text[::-1]:
-#     print("the text is palidrom")
-# else:
-#     print("text is no palidrome")
-
-
-
-
-# find num 5244 = 5*2*4*4
-
-num = 5244
-total = 1
-
-for i in str(num):
-    total *= int(i)
-
-print(total)
+slicename = name[1 : 4]
+print(slicename)
--- variable.py
+++ variable.py
@@ -15,9 +15,26 @@
 # age = 20
 
 # print("your age is", age +5, "now")
-
+#swap two number 
 x = 15
 y = 30
 
 x, y = y, x
 print(x, y)
+# Store your first name and last name in variables and print them together as one full name.
+
+fname ="kalpesh"
+lname = "nivilkar"
+
+fullname = (fname + "" + lname)
+print(fullname)
+
+# Let marks1 = 78, marks2 = 85, marks3 = 90. Store their average in a variable and print it.
+
+marks1 = 78
+marks2 = 85
+marks3 = 90
+
+avarage = (marks1 +marks2+ marks3)/3
+
+print(avarage)
