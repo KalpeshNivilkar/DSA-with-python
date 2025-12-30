783ac9a6be30fd5ad4c4e0712e1fbf4438924fbe
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-12-31 01:24:57 +0530

    add new code

---
+++ 2_leapYear.py
@@ -0,0 +1,15 @@
+
+
+def toCheckIsLeapOrNot(year):
+
+    if (year % 400 == 0) and (year % 100 == 0):
+        print(year,"This is leap year")
+    elif(year % 4 == 0) and (year % 100 != 0):
+        print(year,"This is leap year")
+    else:
+         print(year,"This is not leap year")
+         
+year = int(input("Enter the Year: "))
+toCheckIsLeapOrNot(year)
+        
+
