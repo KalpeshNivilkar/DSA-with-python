9a7ce8eff267db821d81872a3735fa49ab070fe9
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-06 07:36:41 +0530

    modifies

--- 1_climbingStrais.py
+++ 1_climbingStrais.py
@@ -27,6 +27,20 @@ print(climbStrairs(20))"""""
 
 
 
+"""""def climbingStrairs(n):
+    memo = {}
+    def fun(n):
+        if n < 2:
+            return 1
+        if n in memo:
+            return memo[n]
+        memo[n] = fun(n-1) + fun(n-2)
+        return memo[n]
+    
+    return fun(n)
+print(climbingStrairs(30))"""""
+
+
 def climbingStrairs(n):
     memo = {}
     def fun(n):
