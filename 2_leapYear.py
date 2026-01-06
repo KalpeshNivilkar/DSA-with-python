3fa663a3e29040dde0f7f6d98bfcd0d3464a0eda
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-06 08:00:07 +0530

    add new file

--- 2_leapYear.py
+++ 2_leapYear.py
@@ -1,6 +1,6 @@
 
 
-def toCheckIsLeapOrNot(year):
+"""""def toCheckIsLeapOrNot(year):
 
     if (year % 400 == 0) and (year % 100 == 0):
         print(year,"This is leap year")
@@ -10,6 +10,16 @@ def toCheckIsLeapOrNot(year):
          print(year,"This is not leap year")
          
 year = int(input("Enter the Year: "))
-toCheckIsLeapOrNot(year)
+toCheckIsLeapOrNot(year)"""""
         
 
+def leapYear(year):
+    if (year % 400 == 0) and (year % 100 == 0):
+        print(year,"this is leap year")
+    elif(year % 4 == 0) and (year % 100 != 0):
+        print(year,"this is leap year")
+    else:
+        print(year,"this is not leap year")
+
+year = int(input("enter the year : "))
+leapYear(year)
--- 4_checkNumberIsPositiveNegativeOddEvenZero.py
+++ 4_checkNumberIsPositiveNegativeOddEvenZero.py
@@ -1,6 +1,6 @@
 # given number check is positive,negative,odd,even,zero
 
-def checkNumber(n):
+"""""def checkNumber(n):
     if n == 0:
         print(n,"it is zero")
     elif n < 0:
@@ -13,4 +13,21 @@ def checkNumber(n):
         else:
             print(n,"it is odd ")
 
-(checkNumber(28))
+(checkNumber(28))"""""
+
+
+def checkNumber(num):
+    if num == 0:
+        print("this is zero")
+    if num < 0:
+        print("this is negative number")
+    else:
+        print("this is positive number")
+
+        if num % 2 == 0:
+            print("this is even number")
+        else:
+            print("this is odd number")
+
+num = int(input("enter the number : "))
+checkNumber(num)
