050be3c243afe8f9884a30be468af55d46946b64
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-22 11:27:02 +0530

    add code

--- 7_validPerfectSquare.py
+++ 7_validPerfectSquare.py
@@ -10,10 +10,18 @@ print(validSquare(16))"""""
 
 
 
-def validSquare(num):
+"""def validSquare(num):
     for i in range(0,int(num ** 0.5)+ 1):
         if i * i == num:
             return True
     
-    return False
+    return False"""
     
+
+def validSquare(num):
+    for i in range(0,int(num ** 0.5) + 1):
+        if i * i == num:
+            return True
+          
+    return False
+print(validSquare(20))
