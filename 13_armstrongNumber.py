e48f8f2a0234671443c53a557c9c81095fc0b3a1
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-29 12:14:18 +0530

    add code

---
+++ 13_armstrongNumber.py
@@ -0,0 +1,28 @@
+def isArmstrongNumber(num):
+    summ = 0
+    order = len(str(num))
+    temp = num   # store original value
+
+    while temp > 0:
+        digit = temp % 10
+        summ += digit ** order
+        temp //= 10
+
+    return summ == num
+
+print(isArmstrongNumber(153))
+
+def isArmstrongNumber(num):
+    summ = 0
+    order = len(str(num))
+    temp = num
+
+    while temp > 0:
+        digit = temp % 10
+        summ += digit ** order
+        temp //= 10
+    
+    return summ == num
+
+print(isArmstrongNumber(153))
+    
