707ce27b4178ea226ef8809f8a58e101b7260d4e
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-02-01 04:59:33 +0530

    add code

--- 14_sumOFNaturalNumber.py
+++ 14_sumOFNaturalNumber.py
@@ -6,3 +6,11 @@ def sum_of_natural_number(num):
 
 num = int(input("enter the number: "))
 print(sum_of_natural_number(num))
+
+def sumOfNaturalNumber(num):
+    summ = 0
+    for i in range(1,num+1):
+        summ += i
+    return summ
+num = int(input("enter the number:"))
+print(sumOfNaturalNumber(num))
