32d6739ba33a883e09e721b7989859c0bee6ceab
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-02-28 14:08:52 +0530

    add code

--- 12_perfectNumber.py
+++ 12_perfectNumber.py
@@ -1,4 +1,4 @@
-def perfectNumber(num):
+"""def perfectNumber(num):
     result = 0
     for i in range(1,num):
         if num % i == 0:
@@ -7,5 +7,18 @@ def perfectNumber(num):
     if result != num:
         return False
     return True
-print(perfectNumber(6))
+print(perfectNumber(6))"""
 
+
+
+def perfectNum(num):
+    result = 0
+    for i in range(1,num):
+        if num % i == 0:
+            result += i
+
+    if result == num:
+        return True
+    return False
+
+print(perfectNum(15))
--- 15_twoSum.py
+++ 15_twoSum.py
@@ -1,4 +1,4 @@
-def twoSum(num,target):
+'''def twoSum(num,target):
     for i in range(0,len(num)):
         for j in range(i+1,len(num)):
             if num[i] + num[j] == target:
@@ -7,4 +7,17 @@ def twoSum(num,target):
 
 arr = [2,7,11,15]
 target = 9
-print(twoSum(arr,target))
+print(twoSum(arr,target))'''
+
+def two_sum(num ,target):
+    d = dict()
+    for i in range(len(num)):
+        d[num[i]] = i
+    for i in range(len(num)):
+        need = target - num[i]
+        if(need in d.keys() and d[need] != i):
+            return[i,d[need]]
+
+num = [2,7,11,15]
+target = 9
+print(two_sum(num,target))
