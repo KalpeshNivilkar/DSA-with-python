cda179c30b642c48e9416707597717fb205e8f94
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-03-19 00:05:13 +0530

    add code

--- 21_product_of_array_except_self.py
+++ 21_product_of_array_except_self.py
@@ -1,16 +1,29 @@
+# brute-forse-approach
 def productOfArray(arr):
     ans = []
-
     for i in range(len(arr)):
-        product = 1   # reset for each i
-
+        product = 1
         for j in range(len(arr)):
             if i != j:
-                product *= arr[j]   # use value
-
-        ans.append(product)   # append after inner loop
-
+                product *= arr[j]
+        ans.append(product)
     return ans
+arr = [1,2,3,4]
+print(productOfArray(arr))
 
-arr = [1, 2, 3, 4]
+# optimized-approach
+def productOfArray(arr):
+    n = len(arr)
+    ans = [1] * n
+    left = 1
+    for i in range(n):
+        ans[i] = left
+        left *= arr[i]
+     
+    right = 1
+    for i in range(n-1,-1,-1):
+        ans[i] = right
+        right *= arr[i]
+    return ans
+arr = [1,2,3,4]
 print(productOfArray(arr))
