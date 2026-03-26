840f628cd127546adfe82f50b3706974dbb27c1c
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-03-26 13:22:34 +0530

    add code

--- 23_selection_sort.py
+++ 23_selection_sort.py
@@ -51,3 +51,16 @@ def selection_sort(arr):
 
 arr = [10,23,12,43,55,22]
 print(selection_sort(arr))
+
+def selction_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+    return arr
+arr = [10,23,12,43,55,22]
+print(selection_sort(arr))
