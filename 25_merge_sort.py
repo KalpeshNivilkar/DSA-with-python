c045cf111d69902be75c034431c9fa7689409e79
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-04-01 15:21:40 +0530

    updated code

--- 25_merge_sort.py
+++ 25_merge_sort.py
@@ -1,4 +1,4 @@
-def merge_sort(arr):
+"""def merge_sort(arr):
     if len(arr) <= 1:
         return arr
 
@@ -12,9 +12,12 @@ def merge_sort(arr):
 
 def merge(left, right):
     result = []
-    i = j = 0
+    i = 0
+    j = 0
+    l = len(left)
+    r = len(right)
 
-    while i < len(left) and j < len(right):
+    while i < l and j < r:
         if left[i] < right[j]:
             result.append(left[i])
             i += 1
@@ -29,4 +32,34 @@ def merge(left, right):
 
 
 arr = [5, 3, 4, 1]
+print(merge_sort(arr))"""
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge_array(left,right)
+
+def merge_array(left,right):
+    i = 0
+    j = 0
+    result = []
+    l = len(left)
+    r = len(right)
+    while i < l and j < r:
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+arr =[ 4,8,9,3,4]
 print(merge_sort(arr))
