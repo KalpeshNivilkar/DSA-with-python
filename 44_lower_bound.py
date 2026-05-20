77912d891e08d8e356e469e9db69b31d07e13025
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-05-20 12:39:14 +0530

    do chnages in the code

--- 44_lower_bound.py
+++ 44_lower_bound.py
@@ -1,18 +1,40 @@
-def findFloor( arr, k):
-    n = len(arr)
-    lb = -1               # floor index; -1 means no floor yet
-    low, high = 0, n - 1  # binary-search boundaries
+# def findFloor(arr,k):
+#     n = len(arr)
+#     lb = -1               
+#     low, high = 0, n - 1  
+
+#     while low <= high:
+#         mid = (low + high) // 2        
+
+#         if arr[mid] <= k:
+#             lb = mid                  
+#             low = mid + 1              
+#         else:
+#             high = mid - 1             
+
+#     return lb
+# arr = [1, 2, 8, 10, 11]
+# k = 9
+# print(findFloor(arr,k))
+
+
+def lower_bound(nums,target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    lb = -1
 
     while low <= high:
-        mid = (low + high) // 2        # middle index
-
-        if arr[mid] <= k:
-            lb = mid                   # potential (better) floor
-            low = mid + 1              # look on the right side
+        mid = (low + high) // 2
+        if nums[mid] >= target:
+            lb = mid
+            high = mid - 1
         else:
-            high = mid - 1             # look on the left side
-
+            low = mid + 1
     return lb
-arr = [1, 2, 8, 10, 11]
-k = 9
-print(findFloor(arr,k))
+
+nums = [2,4,5,6,7,8,10,13]
+target = 9                                              
+
+print(lower_bound(nums,target))
+
--- 45_upper_bound.py
+++ 45_upper_bound.py
@@ -1,6 +1,6 @@
 def upperBound(arr, k):
     n = len(arr)
-    ans = n
+    up = n
     low = 0
     high = n - 1
 
@@ -8,15 +8,36 @@ def upperBound(arr, k):
         mid = (low + high) // 2
 
         if arr[mid] > k:
-            ans = mid
+            up = mid
             high = mid - 1
         else:
             low = mid + 1
 
-    return ans
+    return up
 
 
 arr = [1, 2, 4, 4, 5, 7]
 k = 4
 
 print(upperBound(arr, k))
+
+
+def upper_bound(nums,target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    up = n
+
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] > target:
+            up = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return up
+
+nums = [1,2,2,2,2,3,3,3,3,4,5,6,7,8]
+target = 2
+
+print(upper_bound(nums,target))
