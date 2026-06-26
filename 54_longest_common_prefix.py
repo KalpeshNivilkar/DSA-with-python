8f2c9a778c50591dbb65b6ef4ab2aeeae9dcf99c
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-06-26 08:28:43 +0530

    add some new code

---
+++ 54_longest_common_prefix.py
@@ -0,0 +1,21 @@
+'''Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"'''
+def longest_common_prefix(strs):
+    if strs == "":
+        return ""
+    result = ""
+    base = strs[0]
+    for i in range(len(base)):
+        for word in strs[1:]:
+            if i == len(base) or word[i] != base[i]:
+                return result
+        result += base[i]
+    return result
+strs = ["flower","flow","flight"]
+print(longest_common_prefix(strs))
+
+
+       
+
