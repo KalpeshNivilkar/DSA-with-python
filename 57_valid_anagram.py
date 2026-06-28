89e97291293289caa6048c673bb39396db16a208
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-06-28 13:50:07 +0530

    update the code

--- 57_valid_anagram.py
+++ 57_valid_anagram.py
@@ -35,3 +35,27 @@ s = "zonjlobp"
 t = "lobpzonj"
 
 print(valid_anagram(s,t))
+
+
+def valid_anagram(s,t):
+    if len(s) != len(t):
+        return False
+    store_dict = {}
+    for ch in s:
+        store_dict[ch] = store_dict.get(ch,0) + 1
+    for ch in t:
+        if ch not in store_dict:
+            return False
+        else:
+            if store_dict[ch] == 0:
+                return False
+            else:
+                store_dict[ch] -= 1
+        return True
+s = "zonjlobp"
+t = "lobpzonj"
+
+print(valid_anagram(s,t))
+
+    
+
