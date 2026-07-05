27a48a415b908bd8e8ffade1d4e6d10cc9533dc6
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-07-06 00:48:39 +0530

    add new file

--- 58_sort_ch_by_freq.py
+++ 58_sort_ch_by_freq.py
@@ -14,9 +14,17 @@ def sort_char(s):
     for ch, freq in sorted_char:
         result += ch * freq
     return result
-   
-    
-        
 
 s = "tree"
 print(sort_char(s))
+
+def sort_char(s):
+    result = ""
+    hash_map = {}
+
+    for ch in s:
+        hash_map[ch] = hash_map.get(ch,0) + 1
+    sorted_char = sorted(hash_map.items(),key = lambda x:x[1],reverse=True)
+    for ch,freq in sorted_char:
+        result += ch * freq
+    return result
