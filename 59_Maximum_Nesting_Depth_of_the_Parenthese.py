d859fbbe0f52066da1f681fda71e653858fed0f9
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-07-06 23:10:45 +0530

    write a practice code

--- 59_Maximum_Nesting_Depth_of_the_Parenthese.py
+++ 59_Maximum_Nesting_Depth_of_the_Parenthese.py
@@ -17,3 +17,18 @@ def maximum_nesting_depth_of_parantheses(s):
     return max_dept
 s = "(1+(2*3)+((8)/4))+1"
 print(maximum_nesting_depth_of_parantheses(s))
+
+
+
+def maximum_nesting_depth(s):
+    curr_dept = 0
+    max_dept = 0
+    for ch in s:
+        if ch == '(':
+            curr_dept += 1
+            max_dept = max(max_dept,curr_dept)
+        elif ch == ')':
+            curr_dept -= 1
+    return max_dept
+s = "(1+(2*3)+((8)/4))+1"
+print(maximum_nesting_depth(s))
