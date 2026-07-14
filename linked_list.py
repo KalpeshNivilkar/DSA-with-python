22d8fe12e08b273101880b8e0ec87455f9b00b13
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-07-14 23:51:15 +0530

    add code

---
+++ linked_list.py
@@ -0,0 +1,92 @@
+'''class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+    
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+print(node1.next.val)'''
+
+'''# singly linked list, append method
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        new_node = Node(val)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            curr = self.head
+
+            while curr.next is not None:
+                curr = curr.next
+
+            curr.next = new_node
+ll = SinglyLinkedList()
+ll.append(10)
+ll.append(20)
+print(ll.head.val)            # 10
+print(ll.head.next.val)       # 20
+print(ll.head.next.next) 
+        
+
+
+        '''
+
+# practice
+# how create a node 
+class Node:
+    def __init__ (self,val):
+        self.val = val
+        self.next = None
+# singlay linked list 
+class singlyLinkedList:
+    def __init__ (self):
+        self.head = None
+
+    def append(self,val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+ll = singlyLinkedList()
+
+# Append nodes
+ll.append(10)
+ll.append(20)
+ll.append(30)
+
+
+current = ll.head
+
+while current:
+    print(current.val, end="->")
+    current = current.next
+
+
+
+
+        
+
+    
+
+
