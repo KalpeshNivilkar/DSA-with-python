7a520e3908a495694751f6e11d29429cedc33179
kalpesh nivilkar <kalpeshnivilkar4@gmail.com>
2026-08-25 20:51:36 +0530

    add code

--- 24_bubble_sort.py
+++ 24_bubble_sort.py
@@ -10,15 +10,13 @@ print(bubble_sort(arr))"""
 
 
 
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_index = i
-        for j in range(i+1,n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        
-        arr[i],arr[min_index] = arr[min_index],arr[i]
-    return arr
-arr = [12,44,22,11,55]
-print(selection_sort(arr))
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(n- i- 1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+
+nums = [10,39,22,40,12]
+print(bubble_sort(nums))
