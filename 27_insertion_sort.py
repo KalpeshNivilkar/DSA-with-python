df8f838e27502ae14b83130abe715d83a464b92a
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-08-31 21:41:56 +0530

    add code in insersion sort

--- 27_insertion_sort.py
+++ 27_insertion_sort.py
@@ -13,3 +13,22 @@ def insertion_sort(arr):
 
 arr = [5, 3, 4, 1]
 print(insertion_sort(arr))
+
+
+
+
+def Insertion_sort(nums):
+    n = len(nums)
+
+    for i in range(1,n):
+        key = nums[i]
+        j = i - 1
+
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+
+    return nums
+nums = [5, 3, 4, 1]
+print(Insertion_sort(nums))
