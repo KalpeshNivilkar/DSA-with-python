9de57b0c7cf5592d4498942edb81b93a234b12c0
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-08-31 19:47:10 +0530

    add practice code inside the selection sort file

--- 23_selection_sort.py
+++ 23_selection_sort.py
@@ -93,3 +93,18 @@ def selection(nums):
     return nums
 nums = [10,15,50,30,20]
 print(selection(nums))
+
+
+
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1,n):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+    return nums
+nums = [10,20,9,11]
+print(selection_sort(nums))
