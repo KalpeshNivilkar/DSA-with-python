c9d8164cb6e186784d780a369ce0323462fc62af
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-01 21:14:53 +0530

    add code

--- 27_insertion_sort.py
+++ 27_insertion_sort.py
@@ -32,3 +32,38 @@ def Insertion_sort(nums):
     return nums
 nums = [5, 3, 4, 1]
 print(Insertion_sort(nums))
+
+
+def insertion_sort(nums):
+    n = len(nums)
+    for i in range(1, n):
+        key = nums[i]
+        j = i - 1
+
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+
+        nums[j + 1] = key
+    return nums
+nums = [5,3,4,1]
+print(insertion_sort(nums))
+
+
+
+def insertion_sort3(nums):
+    n = len(nums)
+    for i in range(1, n):
+        key = nums[i]     #1 idx
+        j = i - 1         #0 idx
+
+        while j >= 0 and nums[j] > key: #nums[0idx] > nums[1idx]
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+    return nums
+nums = [10,30,23,11]
+print(insertion_sort3(nums))
+
+
+
