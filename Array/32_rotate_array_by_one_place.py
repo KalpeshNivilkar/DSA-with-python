650ddd6c6cb5b80b22d7aa2ccdcd194d29583c76
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-10 00:03:37 +0530

    practicing code

--- Array/32_rotate_array_by_one_place.py
+++ Array/32_rotate_array_by_one_place.py
@@ -23,3 +23,17 @@ def rotate_array(nums):
 
 arr = [10,30,29,28,12]
 print(rotate_arr(arr))
+
+
+def rotate_array(nums):
+    n = len(nums)
+
+    temp = nums[0]
+
+    for i in range(1,n):
+        nums[i - 1] = nums[i]
+    nums[n-1] = temp
+
+    return nums
+nums = [10,30,29,28,12]
+print(rotate_array(nums))
