c48b8b0f724beeb0f40b6d1c555c12b640357f9b
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-25 02:10:58 +0530

    add

--- 15_twoSum.py
+++ 15_twoSum.py
@@ -21,3 +21,30 @@ def two_sum(num ,target):
 num = [2,7,11,15]
 target = 9
 print(two_sum(num,target))
+
+# brute_force_approach
+def two_sum(nums,target):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(1,n):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+
+num = [2,7,11,15]
+target = 9
+print(two_sum(num,target))
+
+# optimal_appatoach
+def two_sum_2(nums,target):
+    n = len(nums)
+    hash_map = {}
+    for i in range(n):
+        remaining = target - nums[i]
+        if remaining in hash_map:
+            return [hash_map[remaining],i]
+        hash_map[nums[i]] = i
+    return []
+num = [2,7,11,15]
+target = 9
+print(two_sum_2(num,target))
