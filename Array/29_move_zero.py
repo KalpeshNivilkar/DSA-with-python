d2280fdd58c11aa2e179434bb4ba54cb5d1053e6
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-25 01:09:53 +0530

    solve move zero code

--- Array/29_move_zero.py
+++ Array/29_move_zero.py
@@ -17,3 +17,18 @@ def move_zero(arr):
     return arr
 arr = [0,1,0,2,15]
 print(move_zero(arr))
+
+
+
+def move_zero(nums):
+    n = len(nums)
+    i = 0
+    for j in range(1,n):
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j],nums[i]
+            i += 1
+       
+    return nums
+
+nums = [0,1,0,2,15]
+print(move_zero(nums))
